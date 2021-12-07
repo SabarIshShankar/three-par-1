a1ad1bb74c95ffb13a07a9e2be12a5dfccaa53f2
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-07 16:33:06 +0530

    concat

--- script.js
+++ script.js
@@ -498,3 +498,26 @@ THREE.BAS.BaseAnimationMaterial.prototype._concatTransformNormal = function () {
 THREE.BAS.BaseAnimationMaterial.prototype._concatTranformPosition = function () {
   return this.shaderTransformPosition.join("\n");
 };
+
+THREE.BAS.BaseAnimationMaterial.prototype.setUniformValues = function (values) {
+  for (var key in values) {
+    if (key in this.uniforms) {
+      var uniform = this.uniforms[key];
+      var value = values[key];
+
+      switch (uniform.type) {
+        case "c": //color
+          uniform.value.set(value);
+          break;
+        case "v2": //vectors
+        case "v3":
+        case "v4":
+          uniform.value.copy(value);
+          break;
+        case "f": //float
+        case "t": //texture
+          uniform.value = value;
+      }
+    }
+  }
+};
