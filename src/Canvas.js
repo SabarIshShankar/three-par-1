866df480c9a3c7f9b9b7390e82cb1ca34ddbb8a1
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-02-14 12:07:15 +0530

    canvas render mesh

--- src/Canvas.js
+++ src/Canvas.js
@@ -20,5 +20,17 @@ function Switch({ x, set }) {
   const pZ = x.to([0, 1], [-1.2, 1.2]);
   const rX = x.to([0, 1], [0, Math.PI * 1.3]);
   const color = x.to([0, 1], ["#888", "#2a2a2a"]);
-  return <group scale={[1.25, 1.25, 1.25]} dispose="null"></group>;
+  return (
+  <group scale={[1.25, 1.25, 1.25]} dispose={null}>
+    <a.mesh receiveShadow castShadow material={materials.track} geometry={nodes.Cube.geometry} material-color={color} material-roughness={0.5} material-metalness={0.8}/>
+    <a.group position-y={0.85} position-z={pZ}>
+      <a.mesh receiveShadow castShadow raycast={meshBounds} rotation-x={rX} onClick={onClick} onPointerOver={onPointerOver} onPointerOut = {onPointerOut}>
+        <sphereGeometry args={[0.8, 64, 64]}/>
+        <a.meshStandardMaterial roughness={0.5} map={texture}/>
+      </a.mesh>
+      <a.pointLight intensity={100} distance={1.4} color={color}/>
+      <Shadow renderOrder={-1000} position={[0, -1, 0]} rotation={[-Math.PI/2, 0, 0]} scale={1.5}/>
+    </a.group>
+  </group>
+  );
 }
