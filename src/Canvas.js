a566d1efd2e51aff82f5f8610fcfca8268cf7f3d
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-02-14 12:16:23 +0530

    scene light

--- src/Canvas.js
+++ src/Canvas.js
@@ -21,16 +21,76 @@ function Switch({ x, set }) {
   const rX = x.to([0, 1], [0, Math.PI * 1.3]);
   const color = x.to([0, 1], ["#888", "#2a2a2a"]);
   return (
-  <group scale={[1.25, 1.25, 1.25]} dispose={null}>
-    <a.mesh receiveShadow castShadow material={materials.track} geometry={nodes.Cube.geometry} material-color={color} material-roughness={0.5} material-metalness={0.8}/>
-    <a.group position-y={0.85} position-z={pZ}>
-      <a.mesh receiveShadow castShadow raycast={meshBounds} rotation-x={rX} onClick={onClick} onPointerOver={onPointerOver} onPointerOut = {onPointerOut}>
-        <sphereGeometry args={[0.8, 64, 64]}/>
-        <a.meshStandardMaterial roughness={0.5} map={texture}/>
-      </a.mesh>
-      <a.pointLight intensity={100} distance={1.4} color={color}/>
-      <Shadow renderOrder={-1000} position={[0, -1, 0]} rotation={[-Math.PI/2, 0, 0]} scale={1.5}/>
-    </a.group>
-  </group>
+    <group scale={[1.25, 1.25, 1.25]} dispose={null}>
+      <a.mesh
+        receiveShadow
+        castShadow
+        material={materials.track}
+        geometry={nodes.Cube.geometry}
+        material-color={color}
+        material-roughness={0.5}
+        material-metalness={0.8}
+      />
+      <a.group position-y={0.85} position-z={pZ}>
+        <a.mesh
+          receiveShadow
+          castShadow
+          raycast={meshBounds}
+          rotation-x={rX}
+          onClick={onClick}
+          onPointerOver={onPointerOver}
+          onPointerOut={onPointerOut}
+        >
+          <sphereGeometry args={[0.8, 64, 64]} />
+          <a.meshStandardMaterial roughness={0.5} map={texture} />
+        </a.mesh>
+        <a.pointLight intensity={100} distance={1.4} color={color} />
+        <Shadow
+          renderOrder={-1000}
+          position={[0, -1, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={1.5}
+        />
+      </a.group>
+    </group>
+  );
+}
+
+export function Scene({ x, set }) {
+  const color = x.to([0, 1], ["#7fffd4", "c72f46"]);
+  return (
+    <Canvas
+      orthigraphic
+      shadows
+      dpr={[1, 2]}
+      camera={{ zoom: 60, position: [-10, 10, -10], fov: 35 }}
+    >
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[-20, 20, 20]} intensity={1} />
+      <a.directionalLight
+        position={[-20, -20, -20]}
+        intensity={0.5}
+        color={color}
+      />
+      <a.pointLight
+        position={[0, 0, 5]}
+        distance={5}
+        intensity={5}
+        color={color}
+      />
+      <a.spotLight
+        color={color}
+        position={[10, 20, 20]}
+        angle={0.1}
+        intensity={2}
+        shadow-mapsize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-bias={0 - 0.0001}
+        castShadow
+      />
+      <Suspense fallback={null}>
+        <Switch x={x} set={set} />
+      </Suspense>
+    </Canvas>
   );
 }
